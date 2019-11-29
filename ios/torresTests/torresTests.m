d222bbf8db274b5b455388782aa62a195eaa276d
Luis Villamil <villamil_50@hotmail.com>
2019-11-29 12:40:32 -0600

    Rename application

--- ios/torresTests/torresTests.m
+++ ios/torresTests/torresTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface torresTests : XCTestCase
+@interface TorresTests : XCTestCase
 
 @end
 
-@implementation torresTests
+@implementation TorresTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
